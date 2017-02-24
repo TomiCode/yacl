912265ea98b4ec53448841b965bbe32ef28f506e
Tomek K <tomicode@gmail.com>
2017-02-25 00:08:52 +0100

    This a commit. Wow. Nice!

--- yacl/yacl.c
+++ yacl/yacl.c
@@ -1,5 +1,9 @@
 #include "yacl.h"
 
+#define YACL_CURRENT_NONE      0x01
+#define YACL_CURRENT_NAMESPACE 0x02
+#define YACL_CURRENT_VARIABLE  0x04
+
 struct yacl_varlist {
   yacl_variable *pnext;
   yacl_variable *pprev;
@@ -36,14 +40,15 @@ yacl_handle yacl_allocate_next(struct yacl_namespace ** p_namespace)
 yacl_handle yacl_create(FILE* config_file, yacl_descriptor* config_desc)
 {
   struct yacl_namespace* current_namespace = NULL;
+  char yacl_parser_current    = YACL_CURRENT_NONE;
+  char yacl_parser_array[128] = {0};
+  int  yacl_parser_position   = 0;
+
   yacl_handle handle = yacl_allocate_next(&current_namespace);
+  printf("Create new handle: %d.\n", handle);
 
-  printf("New handle: %d.\n", handle);
+  for (char r = '\0'; fread(&r, sizeof(char), 1, config_file) == 1;) {
 
-  char r = '\0';
-  while (fread(&r, sizeof(char), 1, config_file) == 1) {
-    printf("%c", r);
   }
-
   return handle;
 }
